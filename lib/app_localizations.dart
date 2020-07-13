a4fb188d3e2bec3cd09e64a85c8357c34a7e7bf4
Quqlock <quqlock@gmail.com>
2020-07-13 17:00:40 +0200

    alpha with langs

---
+++ lib/app_localizations.dart
@@ -0,0 +1,31 @@
+import 'dart:async';
+import 'dart:convert';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class AppLocalizations {
+  final Locale appLocale;
+
+  AppLocalizations(this.appLocale);
+
+  static AppLocalizations of(BuildContext context) {
+    return Localizations.of<AppLocalizations>(context, AppLocalizations);
+  }
+
+  Map<String, String> _localizedStrings;
+
+  Future<bool> load() async {
+    //load JSON file
+    String jsonString =
+        await rootBundle.loadString('i18n/${appLocale.languageCode}.json');
+    Map<String, dynamic> jsonMap = json.decode(jsonString);
+    _localizedStrings = jsonMap.map((key, value) {
+      return MapEntry(key, value.toString());
+    });
+    return true;
+  }
+
+  String translate(String jsonkey) {
+    return _localizedStrings[jsonkey];
+  }
+}
